a9494b1182e2dd02ae2124e320950da6154fb416
Manas Pati <patimanas321@yahoo.com>
2022-08-08 23:26:06 +0530

    DataGrid header row

--- src/Constants/AppConstants.js
+++ src/Constants/AppConstants.js
@@ -1,5 +1,9 @@
 const AppConstants = {
-  BASE_API_URL: process.env.REACT_APP_API_URL
+  BASE_API_URL: process.env.REACT_APP_API_URL,
+  SORT_ORDER: {
+    Ascending: 'asc',
+    Descending: 'desc'
+  },
 };
 
 export default AppConstants;
