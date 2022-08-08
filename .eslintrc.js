c713eef4cefc65291e923e4850c46dcd4de7a9d9
Manas Pati <patimanas321@yahoo.com>
2022-08-08 22:26:06 +0530

    Lint fixes

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,36 +1,37 @@
 module.exports = {
-    env: {
-      browser: true,
-      es2021: true
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true
+  },
+  extends: [
+    'plugin:react/recommended',
+    'standard'
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
     },
-    extends: [
-      'plugin:react/recommended',
-      'standard'
-    ],
-    parserOptions: {
-      ecmaFeatures: {
-        jsx: true
-      },
-      ecmaVersion: 'latest',
-      sourceType: 'module'
-    },
-    plugins: [
-      'react'
-    ],
-    rules: {
-      semi: [2, 'always'],
-      indent: ['error', 2]
-    },
-    overrides: [
-      {
-        files: [
-          '**/*.spec.js',
-          '**/*.spec.jsx'
-        ],
-        env: {
-          jest: true
-        }
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: [
+    'react'
+  ],
+  rules: {
+    semi: [2, 'always'],
+    indent: ['error', 2],
+    'react/react-in-jsx-scope': 'off'
+  },
+  overrides: [
+    {
+      files: [
+        '**/*.spec.js',
+        '**/*.spec.jsx'
+      ],
+      env: {
+        jest: true
       }
-    ]
-  };
-  
+    }
+  ]
+};
--- db/server.js
+++ db/server.js
@@ -1,35 +1,35 @@
-const jsonServer = require('json-server')
+const jsonServer = require('json-server');
 const server = jsonServer.create();
 
 const generateRandomNumber = (min = -3, max = 5) => {
-    const random = Math.random() * (+max - +min) + +min; 
-    return Math.floor(random);
-}
+  const random = Math.random() * (+max - +min) + +min;
+  return Math.floor(random);
+};
 const generateRandomMaturityDate = () => {
-    let date = new Date();
-    date.setDate(date.getDate() + generateRandomNumber());
-    return date;
+  const date = new Date();
+  date.setDate(date.getDate() + generateRandomNumber());
+  return date;
 };
 const generateNewTrades = () => {
-    const res = [];
-    for (let i = 0; i < 10; i++) {
-        res.push({
-            id: `TRADE_${generateRandomNumber(1, 1000).toString().padStart(4, '0')}`,
-            version: generateRandomNumber(1, 10),
-            counterPartyId: `Counter-Party-${i}`,
-            bookingId: `B${generateRandomNumber(1, 10000).toString().padStart(5, '0')}`,
-            maturityDate: generateRandomMaturityDate(),
-            createdDate: new Date()
-        });
-    }
+  const res = [];
+  for (let i = 0; i < 10; i++) {
+    res.push({
+      id: `TRADE_${generateRandomNumber(1, 1000).toString().padStart(4, '0')}`,
+      version: generateRandomNumber(1, 10),
+      counterPartyId: `Counter-Party-${i}`,
+      bookingId: `B${generateRandomNumber(1, 10000).toString().padStart(5, '0')}`,
+      maturityDate: generateRandomMaturityDate(),
+      createdDate: new Date()
+    });
+  }
 
-    return res;
+  return res;
 };
 
 server.get('/trades', (req, res) => {
-    res.jsonp(generateNewTrades());
+  res.jsonp(generateNewTrades());
 });
 
 server.listen(3001, () => {
-  console.log('JSON Server is running')
-})
+  console.log('JSON Server is running');
+});
