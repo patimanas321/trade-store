b3b1fe30401bb3e1f01a0e7b7f0f525c8954e546
Manas Pati <patimanas321@yahoo.com>
2022-08-08 20:13:51 +0530

    eslint setup

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+    env: {
+      browser: true,
+      es2021: true
+    },
+    extends: [
+      'plugin:react/recommended',
+      'standard'
+    ],
+    parserOptions: {
+      ecmaFeatures: {
+        jsx: true
+      },
+      ecmaVersion: 'latest',
+      sourceType: 'module'
+    },
+    plugins: [
+      'react'
+    ],
+    rules: {
+      semi: [2, 'always'],
+      indent: ['error', 2]
+    },
+    overrides: [
+      {
+        files: [
+          '**/*.spec.js',
+          '**/*.spec.jsx'
+        ],
+        env: {
+          jest: true
+        }
+      }
+    ]
+  };
+  
