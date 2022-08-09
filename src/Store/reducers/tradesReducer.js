edf8b529cdbc330e5d1d8d2451afd33c051debc7
Manas Pati <patimanas321@yahoo.com>
2022-08-09 11:14:55 +0530

    Trade Util

--- src/Store/reducers/tradesReducer.js
+++ src/Store/reducers/tradesReducer.js
@@ -1,4 +1,5 @@
 import { GET_LATEST_TRADES, SET_SORT_MODEL } from '../actionTypes';
+import { mergeTrades, sortTrades } from '../../Utils/trade';
 import AppConstants from '../../Constants/AppConstants';
 
 const INITIAL_STATE = {
@@ -6,31 +7,45 @@ const INITIAL_STATE = {
   sortModel: {
     column: AppConstants.TRADE_COLS.ID,
     order: AppConstants.SORT_ORDER.ASCENDING
+  },
+  lastUpdate: {
+    updatedAt: null,
+    lowerVersionCount: 0,
+    sameVersionCount: 0,
+    lessMaturityDateCount: 0,
+    validRecordsCount: 0
   }
-};
-
-const sortRecords = (records, { column, order }) => {
-  return records.sort((a, b) => {
-    if (order === AppConstants.SORT_ORDER.ASCENDING) {
-      return a[column] > b[column] ? 1 : -1;
-    }
-    return a[column] > b[column] ? -1 : 1;
-  });
 };
 
 const reducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
-  case GET_LATEST_TRADES:
+  case GET_LATEST_TRADES: {
+    const {
+      trades,
+      lowerVersionCount,
+      sameVersionCount,
+      lessMaturityDateCount,
+      validRecordsCount
+    } = mergeTrades(state.list, action.data);
+
     return {
       ...state,
-      list: [...state.list, ...action.data]
+      list: sortTrades(trades, state.sortModel),
+      lastUpdate: {
+        updatedAt: new Date(),
+        lowerVersionCount,
+        sameVersionCount,
+        lessMaturityDateCount,
+        validRecordsCount
+      }
     };
+  }
 
   case SET_SORT_MODEL:
     return {
       ...state,
       sortModel: action.data,
-      list: sortRecords(state.list, action.data)
+      list: sortTrades(state.list, action.data)
     };
 
   default: return state;
---
+++ src/Utils/trade.js
@@ -0,0 +1,53 @@
+import AppConstants from '../Constants/AppConstants';
+
+export function sortTrades (records, { column, order }) {
+  return records.sort((a, b) => {
+    if (order === AppConstants.SORT_ORDER.ASCENDING) {
+      return a[column] > b[column] ? 1 : -1;
+    }
+    return a[column] > b[column] ? -1 : 1;
+  });
+};
+
+export function mergeTrades (list, newTrades) {
+  const copy = [...list];
+  let lowerVersionCount = 0;
+  let sameVersionCount = 0;
+  let lessMaturityDateCount = 0;
+  let validRecordsCount = 0;
+  const today = new Date();
+  today.setHours(0, 0, 0, 0);
+
+  for (const trade of newTrades) {
+    // Maturity date less then today - REJECT
+    if (trade.maturityDate < today) {
+      lessMaturityDateCount++;
+      continue;
+    }
+    // If same version - OVERRIDE
+    const matchingIdAndVersionIndex = copy.indexOf(entry => entry.id === trade.id && entry.version === trade.version);
+    if (matchingIdAndVersionIndex > -1) {
+      copy[matchingIdAndVersionIndex] = trade;
+      sameVersionCount++;
+      continue;
+    }
+    // If smaller version - REJECT
+    const allRecordsWithSameID = copy.filter(entry => entry.id === trade.id).sort((a, b) => b.version - a.version);
+    if (allRecordsWithSameID?.[0]?.version > trade.version) {
+      lowerVersionCount++;
+      continue;
+    }
+
+    // Valid Record - ADD
+    copy.push(trade);
+    validRecordsCount++;
+  }
+
+  return {
+    trades: copy,
+    lowerVersionCount,
+    sameVersionCount,
+    lessMaturityDateCount,
+    validRecordsCount
+  };
+};
